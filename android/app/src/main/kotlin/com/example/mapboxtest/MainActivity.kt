60ed8f5fa9cb3e27f96d36e36446da13986ec843
AhmedZaghdoudi <zaghdoudiahmed46@gmail.com>
2023-03-29 13:49:30 +0100

    push2

--- android/app/src/main/kotlin/com/example/mapboxtest/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mapboxtest/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mapboxtest
-
 import io.flutter.embedding.android.FlutterActivity
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity: FlutterActivity() {
 }
